b843596188a796b12dafffbd1a2aba710dde7e1d
Bilal <bilal@elkhattouti.com>
2019-11-11 18:18:12 +0100

    first models

---
+++ PhoneBook_Models/Contact.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PhoneBook_Models
+{
+	public class Contact
+	{
+		public int Id { get; set; }
+		[Display(Name = "First name"), Required(ErrorMessage = "First name is required")]
+		public string FirstName { get; set; }
+		[Display(Name = "Last name"), Required(ErrorMessage = "Last name is required")]
+		public string LastName { get; set; }
+	}
+}
